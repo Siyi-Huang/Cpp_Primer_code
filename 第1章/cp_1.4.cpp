cc3f73c35c8567c6f3630e6e035eb64ef0bff7c7
siyihuang <2829955641@qq.com>
2021-01-24 14:56:10 +0800

    1.2节练习完成

---
+++ 第1章/cp_1.4.cpp
@@ -0,0 +1,15 @@
+/*
+练习1.4
+编写程序使用乘法运算符*，来打印两个数的和
+*/
+
+#include <iostream>
+
+int main(){
+    std::cout << "Enter two numbers" << std::endl;
+    int val1 = 0, val2 = 0;
+    std::cin >> val1 >> val2;
+    std::cout << "The product of " << val1 << " and " << val2
+              << " is " << val1 * val2 << std::endl;
+    return 0;
+}
---
+++ 第1章/cp_1.5.cpp
@@ -0,0 +1,16 @@
+/*
+练习1.5:
+重写程序，将每个运算对象的打印操作放在一条独立的语句中
+*/
+
+#include <iostream>
+
+int main(){
+    std::cout << "Enter two numbers" << std::endl;
+    int val1 = 0, val2 = 0;
+    std::cin >> val1 >> val2;
+    std::cout << "The product of " << val1;
+    std::cout << " and " << val2;
+    std::cout << " is " << val1 * val2 << std::endl;
+    return 0;
+}
